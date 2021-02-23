bf9e0237d89daefd167b7c8df0190b482891fdab
MatildaMared <matildamared@live.se>
2021-02-23 08:50:47 +0100

    add functionality for reset button

--- script.js
+++ script.js
@@ -12,23 +12,39 @@ const btnNew = document.querySelector(".btn--new");
 const btnHold = document.querySelector(".btn--hold");
 const btnRoll = document.querySelector(".btn--roll");
 
-// Setting starting scores to 0 and hiding dice
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// Declaring variables
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Setting starting conditions
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    diceEl.classList.add('hidden');
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+}
+
+// Starting game
+init();
+
+// Switching player
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
-}
+};
 
 // Rolling the dice
 btnRoll.addEventListener("click", function () {
@@ -79,18 +95,5 @@ btnHold.addEventListener("click", function () {
 })
 
 btnNew.addEventListener("click", function () {
-    player0El.classList.remove("player--winner");
-    player1El.classList.remove("player--winner");
-    player0El.classList.add("player--active");
-    player1El.classList.remove('player--active');
-    diceEl.classList.add('hidden');
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    scores[0] = 0;
-    scores[1] = 0;
-    currentScore = 0;
-    activePlayer = 0;
-    playing = true;
+    init();
 })
